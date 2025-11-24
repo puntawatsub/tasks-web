a2b5897389de58a059396db8e75d277c0bc65d1c
Puntawat Subhamani <puntawatsub@gmail.com>
2025-11-24 10:47:46 +0200

    mon morning 1 week5

---
+++ week6/react-auth/frontend/src/pages/LoginComponent.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const LoginComponent = ({ setIsAuthenticated }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      // endpoint: POST /api/user/login
+      const result = await fetch("/api/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      if (result.ok) {
+        const user = await result.json();
+        sessionStorage.setItem("user", JSON.stringify(user));
+        setIsAuthenticated(true);
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <label>
+        Email:
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <button onClick={handleLogin}>Log In</button>
+    </div>
+  );
+};
+
+export default LoginComponent;
